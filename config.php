7c824da2990c98c845a83c84e912a47033325340
mohamed-aziz-arfaoui <74879280+mohamed-aziz-arfaoui@users.noreply.github.com>
2021-01-05 02:20:22 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     public static function getConnexion() {
       if (!isset(self::$pdo)) {
         try{
-          self::$pdo = new PDO('mysql:host=localhost;dbname=utilisateur', 'root', '',
+          self::$pdo = new PDO('mysql:host=localhost;dbname=aziz', 'root', '',
           [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
